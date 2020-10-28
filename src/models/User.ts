c49776e058d0fe493642a3785d2ef866add406a5
Jaeseong-CS <shimjs8@naver.com>
2020-10-29 00:35:19 +0900

    feat: user model

---
+++ src/models/User.ts
@@ -0,0 +1,17 @@
+import uniqueValidator from 'mongoose-unique-validator';
+import { createSchema, Type, typedModel } from 'ts-mongoose';
+
+const User = createSchema(
+  {
+    name: Type.string({ required: true, unique: true, trim: true }),
+    id: Type.string({ required: true, unique: true }),
+    password: Type.string({ required: true }),
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  },
+);
+User.plugin(uniqueValidator);
+
+export default typedModel('User', User);
