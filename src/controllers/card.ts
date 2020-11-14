569f331fcda448ab2235d5afa3d74ca2a3ef7e51
Jaeseong Sim <shimjs8@naver.com>
2020-11-14 22:16:42 +0900

    feat: card controller

---
+++ src/controllers/card.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+
+import { AuthRequest } from '../middleware/auth';
+import { Card } from '../models';
+
+export const get = async (req: Request, res: Response) => {
+  const { page } = req.params;
+  const cardList = await Card.paginate({}, { page, perPage: 18 });
+  res.status(200).send(cardList);
+};
+
+export const write = async (req: AuthRequest, res: Response) => {
+  const { token } = req;
+  const { title, content } = req.body;
+  const card = new Card({
+    title,
+    content,
+    writer: token?.name,
+  });
+  await card.save();
+  res.sendStatus(200);
+};
